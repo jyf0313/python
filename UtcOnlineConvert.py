3adb093f93c26a98cbc7c0ccfac8a924da6ad4d9
Jiang <yjiang14@yfve.com.cn>
2019-08-05 16:24:11 +0800

    add a python file UtcOnlineConvert.py

---
+++ UtcOnlineConvert.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 13:00:38 2019
+
+@author: YJIANG14
+"""
+
+from datetime import datetime
+
+utc = 1558321826
+
+print(utc)
+
+print(datetime.fromtimestamp(utc))
+
